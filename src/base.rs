b08ada76eed64b28fa7d15e96cf1469866e5d04f
Simon Goller <neosam+github@posteo.de>
2016-11-04 22:48:55 +0100

    Started a little doc strings.

--- src/base.rs
+++ src/base.rs
@@ -1,5 +1,7 @@
+/// Internal mesh identifier 
 pub type Identifier = i32;
 
+/// A type which return a mesh identifier
 pub trait Id {
 	fn get_id(&self) -> Identifier;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! Procedural 3d mesh generation
+//!
+//! This library tries to provide tools to create mesh tools
+//! the procedural way.
+
 pub mod base;
 pub mod vector;
 pub mod vertex;
